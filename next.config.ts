96ab85c10d273c7b9f70599060b932d106a1b3c8
Shadik Mahamud <96811195+shadikmahamudsohan@users.noreply.github.com>
2024-12-11 15:29:18 +0000

    auth

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+        serverActions: {
+          allowedOrigins: [
+            'localhost:3000', // localhost
+            'https://animated-space-robot-qrr95qr5rq294wq-3000.app.github.dev', // Codespaces
+          ],
+        },
+      },
 };
 
 export default nextConfig;
